44ab2d37a50ef9584bbfc80e650d02b49e96b229
Lucas Lopes <l.lopes.fga@gmail.com>
2019-09-01 21:51:45 -0300

    Deleted test.js
    Co-authored-by: Damarcones <damarcones@gmail.com>

--- test.js
+++
@@ -1,9 +0,0 @@
-const { insertionsort, indexsearch, menu , binsearch, buscaindex} = require('./function')
-const DADOS = require("./pessoas")
-const dados = insertionsort(DADOS)
-vindex = indexsearch(dados, 10);
-if(dados[1].cpf === "042.682.896-82")
-    console.log("ok")
-let res = binsearch(dados,"042.682.896-82")
-console.log(dados[res])
-// buscaindex(vindex,dados, "123.456.789-00")
